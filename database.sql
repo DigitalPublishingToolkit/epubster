f5db950a213aa199c7350c1e48e1050db513cc00
Marc <mensch@heutediewelt.nl>
2014-09-21 14:54:54 +0200

    Updated database schema

--- database.sql
+++ database.sql
@@ -16,17 +16,82 @@ CREATE TABLE `editions` (
   `id` bigint(11) unsigned NOT NULL AUTO_INCREMENT,
   `name` tinytext,
   `author` varchar(255) DEFAULT NULL,
-  `website` varchar(255) DEFAULT NULL,
+  `website` varchar(255) NOT NULL,
   `publisher` varchar(255) DEFAULT NULL,
   `publisher_website` varchar(255) DEFAULT NULL,
-  `identifier` varchar(255) DEFAULT '',
   `description` text,
+  `identifier` varchar(255) NOT NULL,
   `cover` text,
   `style` tinytext,
-  `index` tinyint(1),
-  `timestamp` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `index` tinyint(1) NOT NULL,
+  `timestamp` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table editions_sections
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `editions_sections`;
+
+CREATE TABLE `editions_sections` (
+  `id` bigint(11) unsigned NOT NULL AUTO_INCREMENT,
+  `edition_id` bigint(11) DEFAULT NULL,
+  `section_id` bigint(11) DEFAULT NULL,
+  `timestamp` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table items
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `items`;
+
+CREATE TABLE `items` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `caption` varchar(100) NOT NULL DEFAULT '',
+  `type` varchar(30) NOT NULL DEFAULT '',
+  `size` int(11) DEFAULT NULL,
+  `filename` text,
+  `edition_id` int(11) DEFAULT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table sections
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `sections`;
+
+CREATE TABLE `sections` (
+  `id` bigint(11) unsigned NOT NULL AUTO_INCREMENT,
+  `order` int(11) DEFAULT NULL,
+  `title` tinytext,
+  `text` text,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table users
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) DEFAULT NULL,
+  `email` tinytext,
+  `password` varchar(255) DEFAULT NULL,
+  `timestamp` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
 
